a7c05e51cfc2b1c1b73541c504f9002a04f92644
rainmakerbro <1811383+rainmakerbro@users.noreply.github.com>
2025-02-16 14:46:56 -0500

    initialization

---
+++ bot-env/example-bot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import random
+import json
+import logging
+
+description = '''A great guy who handles stuff you'd want to do during an RPG session'''
+
+intents = discord.Intents.default()
+# intents.members = True
+intents.message_content = True
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+with open('./bot-env/bunkertoken.json') as f:
+    d = json.load(f)
+
+
+
+#bot = discord.Bot(intents=intents)
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    print('----------')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('?hello'):
+        await message.channel.send('Hello!')
+
+@bot.command()
+async def roll(ctx, dice: str):
+    '''Rolls dice in NdN format'''
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+bot.run(f'{d}', log_handler=handler)
